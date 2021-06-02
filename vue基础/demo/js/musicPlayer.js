245b6d81bf76fa352f97d73c231ef01f0403957a
originks <2992720826@qq.com>
2021-06-02 22:50:33 +0800

    music主面板

--- vue基础/demo/js/musicPlayer.js
+++ vue基础/demo/js/musicPlayer.js
@@ -1,10 +1,29 @@
 var musicApp = new Vue({
   el: "#musicApp",
   data: {
-    query: "",
-    
+    query: "张学友",
+    songsList: [],
+    audioUrl: "",
+    songsImg: "https://p2.music.126.net/8cxEF5lBfKLWbAqrF0yVmg==/109951166032666632.jpg",
   },
   methods: {
-
+    querySongs() {
+      var that = this;
+      axios.get("https://autumnfish.cn/search?keywords=" + this.query).then(function (param) {
+        // console.log(param.data.result.songs);
+        that.songsList = param.data.result.songs;
+      })
+    },
+    playSongs(id) {
+      var that = this;
+      axios.get("https://autumnfish.cn/song/url?id=" + id).then(function (param) {
+        console.log(param.data.data[0].url);
+        that.audioUrl = param.data.data[0].url;
+      })
+      axios.get("https://autumnfish.cn/song/detail?ids=" + id).then(function (param) {
+        console.log(param.data.songs[0].al.picUrl);
+        that.songsImg = param.data.songs[0].al.picUrl;
+      })
+    }
   },
 })
